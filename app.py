4be7bd1f4333df310230e2b50fa4581cd5dc355e
666inti <intiyawar321@gmail.com>
2025-06-10 19:50:05 -0400

    nose

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 # Iniciando proyecto
 # from flask import Flask
 
+from flask import Flask, render_template
+from flask import request, redirect,url_for,Blueprint
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("666inti.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
